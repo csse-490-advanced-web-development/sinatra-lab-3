512ab65a83b2ec866ba163d899773c1a1525ebb9
Henry Morin <morinh@rose-hulman.edu>
2024-01-08 14:53:42 -0500

    Finished all 0-27

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,19 +2,22 @@ class TasksController < ApplicationController
   get '/tasks' do
     # Step 9a:
     # Look up all the tasks in the database (by uncommenting the following line
-    # tasks = Task.all
+    tasks = Task.all
     # Step 9b:
     # Pass that list of all tasks to the `erb` partial by adding an extra argument to the erb call below:
     #   locals: { tasks: Task.all }
     # to the end of the line below (similar to what we did with our "Hello, World!" Sinatra app
-    erb :"tasks/index.html"
+    erb :"tasks/index.html", :locals => {tasks: Task.all}
   end
 
 
   # Step 20: Create a new Sinatra route for "/tasks/new"
   #   (see "Routes" in https://sinatrarb.com/intro.html if you need a hint,
   #   or just mimic what we have above for `/tasks`)
-  #
+    get '/tasks/new' do
+      tasks = Task.all
+      erb :"tasks/new.html"    
+    end
   # Step 21: Let the tests prompt you to, add a line to render the erb file
   #   "tasks/new.html" (as before, you can refer to the code we have for `/tasks`)
   #
@@ -36,11 +39,12 @@ class TasksController < ApplicationController
     #
     #          Note: ActiveRecord does sanitize the incoming data for us.
     #
-    # task = Task.new(description: params[:description])
-    # task.save!
+    task = Task.new(description: params[:description])
+    task.save!
     # Step 26b: Since your first test failure is "Not Found", you will start by
     #           uncommenting the following line, to redirect back to the homepage:
-    # redirect "/"
+
+    redirect "/"
 
     # Step 33: Modify the code above so that it uses an if/else statement to
     #          react to the task being valid/invalid.  You should render the 'new'
@@ -55,6 +59,9 @@ class TasksController < ApplicationController
     #          where ... is the full error message.  See the code sample here for
     #          an example of getting full error messages:
     #          https://guides.rubyonrails.org/active_record_validations.html#working-with-validation-errors-errors
+
+
+
   end
 
   # Step 38+:
