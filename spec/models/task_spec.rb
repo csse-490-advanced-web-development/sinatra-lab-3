ba89c9a45e53097528753f1b57606f54685b0b5a
17hsteward <stewarhr@rose-hulman.edu>
2024-01-09 00:42:57 -0500

    All new test pass

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -5,9 +5,6 @@ describe "Task" do
     describe "with valid data" do
       before do
         Task.count.should eq 0  # Sanity check that we haven't got orphaned records hanging around
-        # Step 5 - Let the error "undefined method `count' for Task:Class"
-        #          drive you to the Task model to begin implementation
-        #          (hint: app/models/task.rb)
         Task.create(description: 'Eat breakfast')
       end
 
