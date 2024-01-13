4523c62e3aea7c2d79ec99286c5bc9a8bd695b7f
Henry Morin <tronlover101@gmail.com>
2024-01-13 10:59:41 -0500

    Completed Lab

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -40,7 +40,12 @@ class TasksController < ApplicationController
     #          Note: ActiveRecord does sanitize the incoming data for us.
     #
     task = Task.new(description: params[:description])
-    task.save!
+    if params[:description].blank?
+      redirect "/tasks/new"
+    end
+    if !task.save
+      redirect "/tasks/new"
+    end
     # Step 26b: Since your first test failure is "Not Found", you will start by
     #           uncommenting the following line, to redirect back to the homepage:
     redirect "/"
@@ -69,4 +74,21 @@ class TasksController < ApplicationController
   #   * You will also have to add to this controller so that you can accept PUT requests to e.g. `/tasks/4` (to save updates to the tasks)
   #   * This will give you some good hints on hooking everything together!: https://gist.github.com/victorwhy/45bb5637cd3e7e879ace
   #   * To delete a task: `task.destroy!`
+  get '/tasks/:id' do
+    task = Task.find_by(id: params['id']) 
+    erb :"tasks/edit.html", :locals => {task: task}
+  end
+  delete '/tasks/:id' do
+    task = Task.find_by(id: params['id'])
+    task.destroy
+    redirect "/"
+  end
+  put '/tasks/:id' do
+    task = Task.find_by(id: params['id'])
+    if params[:description].blank?
+      redirect "/tasks/#{params['id']}"
+    end
+    task.update(description: params[:description])
+    redirect "/"
+  end
 end
--- spec/features/managing_todos_spec.rb
+++ spec/features/managing_todos_spec.rb
@@ -94,7 +94,7 @@ feature "Managing Tasks", js: true do
     expect_task_list_to_be_exactly("Correcting my errors works!")
   end
 
-  scenario "updating a todo item with valid data", skip: "Step 36: Unskip this test" do
+  scenario "updating a todo item with valid data", "Step 36: Unskip this test" do
     # We are using a new tool here! Fabricate helps us create valid test data.
     # You can see the definition of this fabricator in ...
     # While the fabricator we defined is very simple now, we will expand it in
@@ -112,7 +112,7 @@ feature "Managing Tasks", js: true do
     expect_task_list_to_be_exactly("Finish Lab 3, finally", "Eat Lunch")
   end
 
-  scenario "updating a todo item with invalid data", skip: "Step 39: Unskip this test" do
+  scenario "updating a todo item with invalid data", "Step 39: Unskip this test" do
     Task.create(description: 'Eat Breakfast')
     visit '/'
     expect_task_list_to_be_exactly("Eat Breakfast")
@@ -128,7 +128,7 @@ feature "Managing Tasks", js: true do
     expect_task_list_to_be_exactly("Correcting my errors works!")
   end
 
-  scenario "deleting a todo" do, skip: "Step 40: Unskip this test" do
+  scenario "deleting a todo", "Step 40: Unskip this test" do
     Task.create(description: 'Eat Breakfast')
     Task.create(description: 'Join class session')
     Task.create(description: 'Finish Lab 3, finally')
