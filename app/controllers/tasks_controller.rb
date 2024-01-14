d29c7e50b2ece7263c4019130a7d3450cf4167ae
KarasuKoi <karasukoi69@gmail.com>
2024-01-13 18:47:16 -0600

    revisited, stuck on step 21 (will have to revisit with help)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,27 +2,18 @@ class TasksController < ApplicationController
   get '/tasks' do
     # Step 9a:
     # Look up all the tasks in the database (by uncommenting the following line
-    # tasks = Task.all
+    tasks = Task.all
     # Step 9b:
     # Pass that list of all tasks to the `erb` partial by adding an extra argument to the erb call below:
     #   locals: { tasks: Task.all }
     # to the end of the line below (similar to what we did with our "Hello, World!" Sinatra app
-    erb :"tasks/index.html"
+    erb :"tasks/index.html", locals: { tasks: Task.all }
   end
 
-
   # Step 20: Create a new Sinatra route for "/tasks/new"
-  #   (see "Routes" in https://sinatrarb.com/intro.html if you need a hint,
-  #   or just mimic what we have above for `/tasks`)
-  #
-  # Step 21: Let the tests prompt you to, add a line to render the erb file
-  #   "tasks/new.html" (as before, you can refer to the code we have for `/tasks`)
-  #
-  #   P.S. Normally, you would have to create the erb view file yourself, but I have
-  #   included it in this commit to save you some frustration
-
-
-
+  get '/tasks/new' do
+    erb :"tasks/new.html"
+  end
 
   # Step 26a: At this point, we need to create the task, and then
   #          redirect back to the homepage so that the user can see
@@ -36,8 +27,8 @@ class TasksController < ApplicationController
     #
     #          Note: ActiveRecord does sanitize the incoming data for us.
     #
-    # task = Task.new(description: params[:description])
-    # task.save!
+    task = Task.new(description: params[:description])
+    task.save!
     # Step 26b: Since your first test failure is "Not Found", you will start by
     #           uncommenting the following line, to redirect back to the homepage:
     # redirect "/"
